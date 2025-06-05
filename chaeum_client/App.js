8bd7dcb96ce37126e7726463d7f7d45c64ee275d
hofkj <carrot070410@naver.com>
2025-06-05 11:02:22 +0900

    프로젝트 생성

---
+++ chaeum_client/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>채움 프론트 개발 시작</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
