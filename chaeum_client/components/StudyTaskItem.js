1087e5b9aa645fe3c06a833985e27e9b98be1b48
hofkj <carrot070410@naver.com>
2025-06-09 01:40:03 +0900

    Feat : StudyTaskItem 제작

---
+++ chaeum_client/components/StudyTaskItem.js
@@ -0,0 +1,72 @@
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+
+export default function StudyTaskItem({ subject = "응용과 개발", time = "00:49:10" }) {     //임시
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.subjectButton}>
+        <Text style={styles.subjectText}>{subject}</Text>
+        <View style={styles.plusCircle}>
+          <Text style={styles.plusText}>＋</Text>
+        </View>
+      </TouchableOpacity>
+
+      <View style={styles.timerSection}>
+        <Text style={styles.timerText}>{time}</Text>
+        <Image
+          source={require("../assets/clock.png")} 
+          style={styles.icon}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 34,
+  },
+  subjectButton: {
+    flexDirection: "row",
+    backgroundColor: "#5399F5",     //버튼 색상 코드 임시
+    borderRadius: 24,
+    paddingHorizontal: 12,
+    paddingVertical: 6.5,
+    alignItems: "center",
+  },
+  subjectText: {
+    color: "#fff",
+    fontSize: 13,
+    marginRight: 6,
+  },
+  plusCircle: {
+    width: 17,
+    height: 17,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  plusText: {
+    color: "#000",
+    fontSize: 13,
+    fontWeight: "400",
+  },
+  timerSection: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  timerText: {
+    color: "#5399F5",
+    fontSize: 14,
+    fontWeight: "400",
+    marginRight: 4,
+  },
+  icon: {
+    width: 15,  //디자인 : 크기 물어보기
+    height: 15,
+    resizeMode: "contain",
+  },
+});
