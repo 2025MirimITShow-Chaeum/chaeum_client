b98550ebe5fe9a875de04a68369f4dc79fd3a59c
hayeong120 <jbg01200249@gmail.com>
2025-06-09 23:28:16 +0900

    🎨design: 코드 수정

--- chaeum_client/components/profile.js
+++ chaeum_client/components/profile.js
@@ -2,7 +2,12 @@ import React from "react";
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { styles } from './styles/profile.styles';
 
-function Profile({ style }) {
+const Profile = ({
+  style,
+  name = '채움이',
+  slogan = '오늘 하루가 미래를 바꾼다.',
+  likes = 0
+}) => {
   return (
     <View style={[styles.container, style]}>
       <View style={styles.profileBox}>
@@ -13,19 +18,19 @@ function Profile({ style }) {
           />
         </View>
         <View style={styles.profile}>
-          <Text style={styles.name}>정세연</Text>
-          <Text style={styles.slogan}>오늘 하루가 미래를 바꾼다.</Text>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.slogan}>{slogan}</Text>
         </View>
       </View>
       
       <View style={styles.reactions}>
         <View style={[styles.reaction, { marginBottom: 4 }]}>
-          <Image source={require('../assets/불.png')} />
+          <Image source={require('../assets/불.png')} style={styles.fire} />
           <Text style={styles.reactionText}>7</Text>
         </View>
         <View style={styles.reaction}>
-          <Image source={require('../assets/하트.png')} />
-          <Text style={styles.reactionText}>24</Text>
+          <Image source={require('../assets/하트.png')} style={styles.heart} />
+          <Text style={styles.reactionText}>{likes}</Text>
         </View>
       </View>
 
